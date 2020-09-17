59b51df64455d398b3fbac9c0dcffca3f0181afb
clicktronix <s28.morgan@gmail.com>
2020-09-18 00:20:52 +0600

    Added additional photos for tests

--- components/Grid/constants.ts
+++ components/Grid/constants.ts
@@ -44,4 +44,19 @@ export const photos = [
     width: 4,
     height: 3,
   },
+  {
+    src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799',
+    width: 1,
+    height: 1,
+  },
+  {
+    src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599',
+    width: 4,
+    height: 3,
+  },
 ];
