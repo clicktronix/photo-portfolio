2432e1125fdeb82457df25b7ff82944feed0843f
clicktronix <s28.morgan@gmail.com>
2020-11-02 15:36:37 +0600

    Deleted login page, added grid page

---
+++ pages/grid.tsx
@@ -0,0 +1,37 @@
+import { Grid } from 'components/Grid/Grid';
+import { CONFIG } from 'core/config';
+import { Photo, PhotoResponse } from 'models/photo';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+
+import { Layout } from '../components/Layout/Layout';
+
+type GridProps = {
+  photos: Photo[];
+};
+
+export default function Home({ photos }: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <Layout withFooter>
+      <Grid photos={photos} />
+    </Layout>
+  );
+}
+
+export const getStaticProps: GetStaticProps<GridProps> = async () => {
+  const data: PhotoResponse[] = await fetch(`${CONFIG.baseUrl}/api/v1/photos/grid`)
+    .then((res) => res.json())
+    .catch((err) => err.json());
+  const photos = data
+    .filter((x) => Boolean(x.is_grid))
+    .map((x) => ({
+      width: x.width,
+      height: x.height,
+      src: x.img,
+    }));
+
+  return {
+    props: {
+      photos,
+    },
+  };
+};
