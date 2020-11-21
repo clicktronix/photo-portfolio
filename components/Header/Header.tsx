1f3adbf098fb323ebf3e96bc08b9e29a71eaf1d2
clicktronix <s28.morgan@gmail.com>
2020-11-21 23:39:37 +0600

    Created first worked iteration of hamburger menu

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+import { HamburgerMenu } from '../HamburgerButton/HamburgerButton';
 import styles from './Header.module.scss';
 
 export function Header() {
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+  const onHamburgerClick = () => {
+    setIsMenuClicked((s) => !s);
+  };
+
   return (
     <header className={styles.Header}>
+      <HamburgerMenu isClicked={isMenuClicked} onClick={onHamburgerClick} />
+
       <nav className={styles.Menu}>
         <a href="1">фото</a>
         <a href="2">альбомы</a>
