4cf1b8b49968dd263bb945f62c98173b3be71503
clicktronix <s28.morgan@gmail.com>
2021-02-02 00:32:10 +0600

    Added keydown and mousemove listeners for lightbox

--- components/LightBox/LightBox.tsx
+++ components/LightBox/LightBox.tsx
@@ -1,5 +1,5 @@
 import { Photo } from 'models/photo';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import cn from 'classnames';
 
 import styles from './LightBox.module.scss';
@@ -12,12 +12,59 @@ type LightBoxProps = {
   onClose?: () => void;
 };
 
+const ESCAPE_KEY = 'Escape';
+const ARROW_RIGHT_KEY = 'ArrowRight';
+const ARROW_LEFT_KEY = 'ArrowLeft';
+
 export function LightBox({ isShow, currentPhoto, photos, onClose }: LightBoxProps) {
   const [photoToShow, setPhotoToShow] = useState<Photo | undefined>(
     photos.find((x) => x.src === currentPhoto.src),
   );
   const [lightBoxDisplay, setLightBoxDisplay] = useState(isShow);
   const [isLoading, setIsLoading] = useState(true);
+  const [isShowControls, setIsShowControls] = useState(false);
+  const mouseMoveTimeOut = useRef<NodeJS.Timeout>();
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  });
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    hideControls();
+    switch (event.key) {
+      case ESCAPE_KEY:
+        hideLightBox();
+        break;
+      case ARROW_RIGHT_KEY:
+        showNext();
+        break;
+      case ARROW_LEFT_KEY:
+        showPrev();
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleMouseMove = () => {
+    clearTimeout(mouseMoveTimeOut.current);
+    showControls();
+    mouseMoveTimeOut.current = setTimeout(hideControls, 1500);
+  };
+
+  const hideControls = () => {
+    setIsShowControls(false);
+  };
+
+  const showControls = () => {
+    setIsShowControls(true);
+  };
 
   const hideLightBox = () => {
     setLightBoxDisplay(false);
@@ -28,8 +75,7 @@ export function LightBox({ isShow, currentPhoto, photos, onClose }: LightBoxProp
     setIsLoading(false);
   };
 
-  const showNext = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.stopPropagation();
+  const showNext = () => {
     const currentIndex = photos.indexOf(photoToShow);
     if (currentIndex >= photos.length - 1) {
       setPhotoToShow(photos[0]);
@@ -39,8 +85,7 @@ export function LightBox({ isShow, currentPhoto, photos, onClose }: LightBoxProp
     }
   };
 
-  const showPrev = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.stopPropagation();
+  const showPrev = () => {
     const currentIndex = photos.indexOf(photoToShow);
     if (currentIndex <= 0) {
       setPhotoToShow(photos[0]);
@@ -57,7 +102,7 @@ export function LightBox({ isShow, currentPhoto, photos, onClose }: LightBoxProp
       })}
     >
       <div className={styles.Wrapper}>
-        {!isLoading && (
+        {isShowControls && (
           <>
             <CloseButton tabIndex={-2} classes={styles.HideButton} onClick={hideLightBox} />
             <span role="button" tabIndex={0} className={styles.PrevButton} onClick={showPrev} />
@@ -71,7 +116,7 @@ export function LightBox({ isShow, currentPhoto, photos, onClose }: LightBoxProp
           src={photoToShow.src}
           onLoad={onPhotoLoad}
         />
-        {!isLoading && (
+        {isShowControls && (
           <span role="button" tabIndex={-1} className={styles.NextButton} onClick={showNext} />
         )}
       </div>
