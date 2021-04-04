04fb8e8d00b0e1bf1c76136ad69676f41f36938a
clicktronix <s28.morgan@gmail.com>
2021-04-04 20:05:15 +0600

    Added contacts api, contacts page updated

--- pages/contacts/index.tsx
+++ pages/contacts/index.tsx
@@ -1,5 +1,45 @@
-import { Layout } from 'components';
+import { GetStaticProps } from 'next';
 
-export default function Contacts() {
-  return <Layout withFooter>Contacts page</Layout>;
+import { Layout } from 'components';
+import { Contacts } from 'models/contacts';
+import { api } from 'services/api';
+import { Error } from 'components/Error/Error';
+
+import styles from './Contacts.module.scss';
+
+type ContactsProps = {
+  contacts?: Contacts;
+  error?: string;
+};
+
+export default function ContactsPage({ contacts, error }: ContactsProps) {
+  if (error) {
+    return <Error title={error} />;
+  }
+
+  return (
+    <Layout withFooter>
+      <div className={styles.Contacts}>
+        <p className={styles.Title}>Кушебина Айгерим</p>
+      </div>
+    </Layout>
+  );
 }
+
+export const getStaticProps: GetStaticProps<ContactsProps> = async () => {
+  try {
+    const contacts = await api.contacts.getContacts();
+
+    return {
+      props: {
+        contacts,
+      },
+    };
+  } catch (err) {
+    return {
+      props: {
+        error: err.message,
+      },
+    };
+  }
+};
