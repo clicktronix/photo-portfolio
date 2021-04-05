a34d02f23b037bf590d39b89124148798543910e
clicktronix <s28.morgan@gmail.com>
2021-04-05 11:00:41 +0600

    Added mockup for contacts page

--- pages/contacts/index.tsx
+++ pages/contacts/index.tsx
@@ -13,14 +13,29 @@ type ContactsProps = {
 };
 
 export default function ContactsPage({ contacts, error }: ContactsProps) {
-  if (error) {
+  if (error || !contacts) {
     return <Error title={error} />;
   }
+
+  const whatsappLink = `https://wa.me/${contacts.whatsapp?.replace('+', '')}`;
+  const emailLink = `mailto:${contacts.email}`;
+  const instagram = `http://instagram.com/_u/${contacts.instagram}/`;
 
   return (
     <Layout withFooter>
       <div className={styles.Contacts}>
         <p className={styles.Title}>Кушебина Айгерим</p>
+        <a href={emailLink} className={styles.Cell} target="blank">
+          {contacts.email}
+        </a>
+        <p className={styles.Cell}>{contacts.telegram}</p>
+        <a href={instagram} className={styles.Cell} target="blank">
+          {contacts.instagram}
+        </a>
+        <p className={styles.Cell}>{contacts.phone}</p>
+        <a href={whatsappLink} className={styles.Cell} target="blank">
+          {contacts.whatsapp}
+        </a>
       </div>
     </Layout>
   );
